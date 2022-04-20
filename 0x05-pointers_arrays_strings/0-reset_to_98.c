d1bdc27a162b3c9cd5ce27a86e6adcb562395762
Kadib20 <chikandubuisi23@gmail.com>
2022-04-20 14:54:26 -0700

    0-reset_to_98.c commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,13 @@
-#​include​ ​"​main.h​"
+#​include​ ​"​main.h​" 
+ ​/*​* 
+ ​ * reset_to_98 - reset argument to 98 
+ ​ * @n: pointer 
+ ​ * Return: void 
+ ​*/ 
 
-/*​*
- ​ * reset_to_98 - reset argument to 98
- ​ * @n: pointer
- ​ * Return: void
- ​*/
-void​ ​reset_to_98​(​int​ *n)
-{
+ ​void​ ​reset_to_98​(​int​ *n) 
+ ​{ 
 
- ​           reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
-}
+ ​        *n = ​98​; 
+
+ ​}
