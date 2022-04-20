ae26e53fce1e1affb818e58af69e3f0f4ca441e9
Kadib20 <chikandubuisi23@gmail.com>
2022-04-20 15:02:22 -0700

    0-reset_to_98.c commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
-#​include​ ​"​main.h​" 
- ​/*​* 
- ​ * reset_to_98 - reset argument to 98 
- ​ * @n: pointer 
- ​ * Return: void 
- ​*/ 
-
- ​void​ ​reset_to_98​(​int​ *n) 
- ​{ 
-
- ​        *n = ​98​; 
-
- ​}
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to 98
+ * @n: parameter to take the value.
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
