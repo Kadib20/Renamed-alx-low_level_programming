a703a9f1429b29d46de4dc7a0af6c6dd4f8f1f99
Kadib20 <chikandubuisi23@gmail.com>
2022-04-24 15:27:06 -0700

    0-strcat.c commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,29 @@
-#include "main.h"
-
 /**
- * *_strcat - function commute srtings (dest and src)
+ * *_strcat - function commute srtings
+ *string src to be appended to string dest
  * Return: return value of dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int x;
-	int y;
+        int x;
+        int y;
 
-	x = 0;
-	y = 0;
+        x = 0;
+        y = 0;
 
-	while (dest[x] != '\0')
-	{
-		x++;
-	}
-	while (src[y] != '\0')
-	{
-		dest[x] = src[y];
-		y++;
-		x++;
-	}
+        while (dest[x] != '\0')
+        {
+                x++;
+        }
+        while (src[y] != '\0')
+        {
+                dest[x] = src[y];
+                y++;
 
-	dest[x] = '\0';
-	return (dest);
+		 x++;
+        }
+
+        dest[x] = '\0';
+        return (dest);
 }
